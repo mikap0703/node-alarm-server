73de46ada5374fc02b153ca6d628c1983a118aff
mikap0703 <mika.petak@gmail.com>
2023-03-04 18:12:47 +0100

    improved logging + new Divera handling

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
-const axios = require('axios').default;
-const MailHandler = require('./email-handler');
+import path from "path";
+import { fileURLToPath } from 'url';
+import {config} from "dotenv";
+config();
+import fs from "fs";
+import chalk from 'chalk';
+import axios from "axios";
+import MailHandler from "./email-handler.js";
 
 class Logger {
     constructor() {
     }
 
     log (type, msg) {
-        console.log(`[${type}] - ${msg}`)
+        let doLog = chalk.red
+        switch (type) {
+            case 'INFO':
+                doLog = chalk.bold.green;
+                break
+            case 'WARN':
+                doLog = chalk.bold.yellow
+                break
+            case 'ERROR':
+                doLog = chalk.bold.yellow
+        }
+        console.log(doLog(`[${type}] - ${msg}`))
+    }
+
+    convertObject (o) {
+        return typeof(o) + ': ' + JSON.stringify(o, null, '\t')
     }
 }
 
@@ -17,11 +35,16 @@ class AlarmHandler {
     constructor() {
         this.logger = new Logger()
         // Konfiguration laden
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
+        console.log(__dirname)
+
         const configFolder = process.env.DEV_CONFIG_PATH || './config';
         const configDir = path.join(__dirname, configFolder);
 
         const mailConfigPath = path.join(configDir, 'mail.json');
         const mailConfig = JSON.parse(fs.readFileSync(mailConfigPath));
+
 
         const serialDmeConfigPath = path.join(configDir, 'serial-dme.json');
         const serialDmeConfig = JSON.parse(fs.readFileSync(serialDmeConfigPath));
@@ -29,9 +52,21 @@ class AlarmHandler {
         const generalConfigPath = path.join(configDir, 'general.json');
         const generalConfig = JSON.parse(fs.readFileSync(generalConfigPath));
 
-        this.api_key = generalConfig.api_key
+        this.logger.log('INFO', 'Konfiguration ' + configDir + ' wurde geladen...')
+        this.doTriggerAlarm = generalConfig.alarm
+
+        if (this.doTriggerAlarm) {
+            this.logger.log('INFO', 'Alarmierung aktiv - Einkommende Alarmierungen werden sofort weitergeleitet!')
+        } else {
+            this.logger.log('WARN', 'Alarmierung nicht aktiv - Einkommende Alarmierungen werden nicht weitergeleitet!')
+        }
+
+        this.apiKey = generalConfig.apiKey
         switch (generalConfig.api) {
             case "Divera":
+                let divera = new DiveraHandler(this.apiKey, this.logger)
+
+                this.triggerAlarm = divera.triggerAlarm
                 this.triggerAlarm = this.triggerDivera;
                 break;
             case "Alamos":
@@ -39,15 +74,21 @@ class AlarmHandler {
                 break;
         }
 
-        this.doTriggerAlarm = generalConfig.alarm
-
         if (generalConfig.mail) {
-            let mail = new MailHandler(this.triggerAlarm, mailConfig, this.logger)
-            mail.startConnection()
+            let mail = new MailHandler(this.handleAlarm, mailConfig, this.logger)
+            //mail.startConnection()
         }
         if (generalConfig.serial_dme) {
             // TODO: Serielle Auswertung
-            console.log("SERIAL DME - Auswertung noch nicht implementiert")
+            this.logger.log('WARN', 'SERIAL DME - Auswertung noch nicht implementiert')
+        }
+    }
+
+    handleAlarm(alarmInfo) {
+        if (!this.doTriggerAlarm) {
+            return "Alarm nicht ausgelöst"
+        } else {
+            this.triggerAlarm(alarmInfo)
         }
     }
 
@@ -61,10 +102,10 @@ class AlarmHandler {
             address: alarmInfo.address
         })
             .then(function (response) {
-                console.log(response.data.success);
+                this.logger.log('INFO', this.logger.convertObject(response.data.success));
             })
             .catch(function (error) {
-                console.log(error);
+                this.logger.log('ERROR', error);
             });
     }
 
@@ -77,11 +118,50 @@ class AlarmHandler {
             address: alarmInfo.address
         })
             .then(function (response) {
-                console.log(response.data.success);
+                this.logger.log('INFO', this.logger.convertObject(response.data.success));
             })
             .catch(function (error) {
-                console.log(error);
+                this.logger.log('ERROR', error);
             });
+    }
+}
+
+class DiveraHandler {
+    constructor(apiKey, logger) {
+        this.apikey = apiKey
+        this.logger = logger
+        this.checkConnection()
+    }
+
+    triggerAlarm(alarmInfo) {
+        if (!this.doTriggerAlarm) return "Alarm nicht ausgelöst"
+        axios.post('https://app.divera247.com/api/alarm', {
+            accesskey: this.apikey,
+            group_ids: [109023], // TEST-GRUPPE
+            title: alarmInfo.title,
+            text: alarmInfo.text,
+            address: alarmInfo.address
+        })
+            .then(function (response) {
+                this.logger.log('INFO', this.logger.convertObject(response.data.success));
+            })
+            .catch(function (error) {
+                this.logger.log('ERROR', error);
+            });
+    }
+
+    checkConnection() {
+        axios.get('https://app.divera247.com/api/v2/pull/all?', { params: {
+            accesskey: this.apikey
+        }}).then((res) => {
+            this.logger.log('INFO', `Divera API erfolgreich authorisiert (${res.data.data.cluster.name})`)
+            let alarmNotificationSettings = res.data.data.cluster.settings.alarm_notification_primary
+            if (alarmNotificationSettings[0] != '') {
+                this.logger.log('INFO', `Es sind ${alarmNotificationSettings.length} Möglichkeiten zur Alarmierung hinterlegt: ${alarmNotificationSettings}`)
+            } else {
+                this.logger.log('WARN', `Es sind keine Kanäle zur Alarmierung eingerichtet. (Divera -> Verwaltung -> Meldungen -> Alarmierungen -> Über diese Kanäle alarmieren)`)
+            }
+        })
     }
 }
 
