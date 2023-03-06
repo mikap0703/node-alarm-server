993531d78f070114697fd08935426de69e45d8b2
mikap0703 <mika.petak@gmail.com>
2023-03-06 23:42:44 +0100

    added apiHandler.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import fs from "fs";
 import chalk from 'chalk';
 import axios from "axios";
 import MailHandler from "./email-handler.js";
+import {DiveraHandler, AlamosHandler} from './apiHandler.js';
 
 class Logger {
     constructor() {
@@ -45,7 +46,6 @@ class AlarmHandler {
         const mailConfigPath = path.join(configDir, 'mail.json');
         const mailConfig = JSON.parse(fs.readFileSync(mailConfigPath));
 
-
         const serialDmeConfigPath = path.join(configDir, 'serial-dme.json');
         const serialDmeConfig = JSON.parse(fs.readFileSync(serialDmeConfigPath));
 
@@ -65,9 +65,7 @@ class AlarmHandler {
         switch (generalConfig.api) {
             case "Divera":
                 let divera = new DiveraHandler(this.apiKey, this.logger)
-
-                this.triggerAlarm = divera.triggerAlarm
-                this.triggerAlarm = this.triggerDivera;
+                this.triggerAlarm = divera.triggerAlarm.bind(divera)
                 break;
             case "Alamos":
                 this.triggerAlarm = this.triggerAlamos;
@@ -75,7 +73,7 @@ class AlarmHandler {
         }
 
         if (generalConfig.mail) {
-            let mail = new MailHandler(this.handleAlarm, mailConfig, this.logger)
+            let mail = new MailHandler(this.handleAlarm.bind(this), mailConfig, this.logger)
             mail.startConnection()
         }
         if (generalConfig.serial_dme) {
@@ -86,65 +84,12 @@ class AlarmHandler {
 
     handleAlarm(alarmInfo) {
         if (!this.doTriggerAlarm) {
-            return "Alarm nicht ausgelöst"
-        } else {
+            this.logger.log('INFO', 'Alarm nicht ausgelöst - Weiterleitung deaktiviert')
+        }
+        else {
             this.triggerAlarm(alarmInfo)
         }
     }
-}
-
-class DiveraHandler {
-    constructor(apiKey, logger) {
-        this.apikey = apiKey
-        this.logger = logger
-        this.checkConnection()
-    }
-
-    triggerAlarm(alarmInfo) {
-        if (!this.doTriggerAlarm) return "Alarm nicht ausgelöst"
-        axios.post('https://app.divera247.com/api/alarm', {
-            accesskey: this.apikey,
-            group_ids: [109023], // TEST-GRUPPE
-            title: alarmInfo.title,
-            text: alarmInfo.text,
-            address: alarmInfo.address
-        })
-            .then(function (response) {
-                this.logger.log('INFO', this.logger.convertObject(response.data.success));
-            })
-            .catch(function (error) {
-                this.logger.log('ERROR', error);
-            });
-    }
-
-    checkConnection() {
-        axios.get('https://app.divera247.com/api/v2/pull/all?', { params: {
-            accesskey: this.apikey
-        }}).then((res) => {
-            this.logger.log('INFO', `Divera API erfolgreich authorisiert (${res.data.data.cluster.name})`)
-            let alarmNotificationSettings = res.data.data.cluster.settings.alarm_notification_primary
-            if (alarmNotificationSettings[0] != '') {
-                this.logger.log('INFO', `Es sind ${alarmNotificationSettings.length} Möglichkeiten zur Alarmierung hinterlegt: ${alarmNotificationSettings}`)
-            } else {
-                this.logger.log('WARN', `Es sind keine Kanäle zur Alarmierung eingerichtet. (Divera -> Verwaltung -> Meldungen -> Alarmierungen -> Über diese Kanäle alarmieren)`)
-            }
-        })
-    }
-}
-
-class AlamosHandler {
-    constructor(apiKey, logger) {
-        this.apikey = apiKey
-        this.logger = logger
-
-        this.logger.log('ERROR', 'Alamos Anbindung ist nicht implementiert')
-    }
-
-    triggerAlarm(alarmInfo) {
-        this.logger.log('ERROR', 'Alamos Anbindung ist nicht implementiert')
-    }
-
-
 }
 
 let alarmhandler = new AlarmHandler()
