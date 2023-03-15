1d05300de8ce6dd7fd45efa671e38487088dbd7f
mikap0703 <mika.petak@gmail.com>
2023-03-15 13:18:31 +0100

    added configChecker

--- config.js
+++ config.js
@@ -1,42 +1,52 @@
-const fs = require('fs');
-const path = require('path');
-require('dotenv').config();
+import * as yup from 'yup';
+import {fileURLToPath} from "url";
+import path from "path";
+import fs from "fs";
 
-const configPath = process.env.DEV_CONFIG_PATH || './config';
-const configDir = path.join(__dirname, configPath);
+import {config} from "dotenv";
+config();
 
-function generalConfiguration() {
-    console.log('Option 1');
-}
+export default class configChecker {
+    constructor(configDir) {
+        this.configDir = configDir
 
-function mailConfiguration() {
-    console.log('Option 2');
-}
+        this.generalConfigSchema = yup.object({
+            api: yup.string().required().oneOf(['Divera', 'Alamos']),
+            apiKey: yup.string().required(),
+            serialDME: yup.boolean().required(),
+            mail: yup.boolean().required(),
+            alarm: yup.boolean().required(),
+        });
 
-function serialConfiguration() {
-    console.log('Option 3');
-}
+        this.mailConfigSchema = yup.object({
+            user: yup.string().required().email().trim().lowercase(),
+            password: yup.string().required()
+        })
 
-console.log("## Konfiguration überprüfen ##")
-console.log('[1] Allgemeine Einstellungen - (general.json)');
-console.log('[2] Mail Einstellungen - (mail.json)');
-console.log('[3] Serielle Schnittstelle Einstellungen - (serial-dme.json)');
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-readline.question('Welche Konfiguration soll überprüft werden? (1, 2 oder 3) ', (index) => {
-    if (index === '1') {
-        generalConfiguration();
-    } else if (index === '2') {
-        mailConfiguration();
-    } else if (index === '3') {
-        serialConfiguration();
-    } else {
-        console.log(`Invalid index: ${index}`);
+        this.serialDMEConfigSchema = yup.object({
+            comPort: yup.string().required(),
+            rics: yup.array()
+        })
     }
 
-    readline.close();
-});
+    async getConfigs() {
+        const mailConfig = JSON.parse(fs.readFileSync(path.join(this.configDir, 'mail.json')))
+        this.mail = await this.mailConfigSchema.validate(mailConfig)
+
+        const serialDmeConfig = JSON.parse(fs.readFileSync(path.join(this.configDir, 'serial-dme.json')))
+        this.serialDME = await this.serialDMEConfigSchema.validate(serialDmeConfig)
+
+        const generalConfig = JSON.parse(fs.readFileSync(path.join(this.configDir, 'general.json')))
+        this.general = await this.generalConfigSchema.validate(generalConfig)
+    }
+}
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const configFolder = process.env.DEV_CONFIG_PATH || './config';
+const configDir = path.join(__dirname, configFolder);
+
+let checker = await new configChecker(configDir)
+await checker.getConfigs()
+
+console.log(checker.mail)
