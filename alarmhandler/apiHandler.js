ae0af2f3de06aef40a315585195c535b556a71a3
mikap0703 <mika.petak@gmail.com>
2023-03-28 00:14:16 +0200

    api is not triggered in dev mode

--- alarmhandler/apiHandler.js
+++ alarmhandler/apiHandler.js
@@ -20,39 +20,44 @@ export class DiveraHandler {
             address = alarmInfo.address.city
         }
 
-        axios.post('https://app.divera247.com/api/v2/alarms', {
-            accesskey: this.apikey,
-            Alarm: {
-                priority: true,
-                notification_type: this.generalConfig.diveraSettings.notificationType, // Empfänger-Auswahl (1 = Ausgewählte Standorte (nur in der PRO-Version), 2 = Alle des Standortes, 3 = Ausgewählte Gruppen, 4 = Ausgewählte Benutzer)
-                foreign_id: alarmInfo.id,
-                title: alarmInfo.title,
-                text: alarmInfo.text,
-                address: address,
-                scene_object: alarmInfo.address.object,
-                group: alarmInfo.groups,
-                vehicle: alarmInfo.vehicles
-            },
-            instructions: {
-                group: {
-                    mapping: "title"
+        if (process.env.DEV_MODE == 1) { // DEV
+            console.log(alarmInfo)
+        }
+        else {
+            axios.post('https://app.divera247.com/api/v2/alarms', {
+                accesskey: this.apikey,
+                Alarm: {
+                    priority: true,
+                    notification_type: this.generalConfig.diveraSettings.notificationType, // Empfänger-Auswahl (1 = Ausgewählte Standorte (nur in der PRO-Version), 2 = Alle des Standortes, 3 = Ausgewählte Gruppen, 4 = Ausgewählte Benutzer)
+                    foreign_id: alarmInfo.id,
+                    title: alarmInfo.title,
+                    text: alarmInfo.text + '\n' + alarmInfo.address.info,
+                    address: address,
+                    scene_object: alarmInfo.address.object,
+                    group: alarmInfo.groups,
+                    vehicle: alarmInfo.vehicles
                 },
-                vehicle: {
-                    mapping: "name"
+                instructions: {
+                    group: {
+                        mapping: "title"
+                    },
+                    vehicle: {
+                        mapping: "name"
+                    }
                 }
-            }
-        })
-        .then((response) => {
-            if (response.data.success) {
-                this.logger.log('INFO', 'Alarm wurde erfolgreich ausgelöst!');
-            }
-            else {
-                this.logger.log('ERROR', 'Beim Auslösen des Alarms ist ein Fehler aufgetreten!');
-            }
-        })
-        .catch((error) => {
-            this.logger.log('ERROR', 'Alarm konnte nicht ausgelöst werden: ' + this.logger.convertObject(error));
-        });
+            })
+                .then((response) => {
+                    if (response.data.success) {
+                        this.logger.log('INFO', 'Alarm wurde erfolgreich ausgelöst!');
+                    }
+                    else {
+                        this.logger.log('ERROR', 'Beim Auslösen des Alarms ist ein Fehler aufgetreten!');
+                    }
+                })
+                .catch((error) => {
+                    this.logger.log('ERROR', 'Alarm konnte nicht ausgelöst werden: ' + this.logger.convertObject(error));
+                });
+        }
     }
 
     checkConnection() {
