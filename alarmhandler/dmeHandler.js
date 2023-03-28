8f1d65136be84e71233194eb917181261aa56876
mikap0703 <mika.petak@gmail.com>
2023-03-28 23:15:10 +0200

    updated dmeHandler

--- alarmhandler/dmeHandler.js
+++ alarmhandler/dmeHandler.js
@@ -1,33 +1,59 @@
-import { SerialPort, ReadlineParser } from 'serialport'
+import { SerialPort, ReadlineParser } from 'serialport';
 
 export default class DmeHandler {
     constructor(triggerAlarm, dmeConfig, logger) {
         this.logger = logger;
         this.config = dmeConfig;
-        this.port = new SerialPort({
-            path: '/dev/tty.usbserial-110',
-            baudRate: 9600,
-            autoOpen: false
-        })
+        this.path = this.config.comPort || '/dev/tty.usbserial-110';
+        this.baudRate = this.config.baudRate || 9600;
+        this.port = new SerialPort( {
+            path: this.path,
+            baudRate: this.baudRate,
+            autoOpen: false,
+        });
         this.parser = new ReadlineParser()
 
         this.port.pipe(this.parser)
+        this.triggerAlarm = triggerAlarm;
     }
 
     start() {
+        if (this.port.isOpen) {
+            this.logger.log('WARN', 'Der serielle Port ist bereits geöffnet.');
+            return;
+        }
         this.port.open((err) => {
             if (err) {
-                return this.logger.log('ERROR', 'Fehler beim Öffnen des Seriellen Ports: ' + err.message)
+                this.logger.log('ERROR', 'Fehler beim Öffnen des seriellen Ports: ' + err.message);
+                return;
             }
-        })
+            this.logger.log('INFO', 'Serieller Port geöffnet: ' + this.path + ', Baudrate: ' + this.baudRate);
+        });
 
         this.parser.on('data', (data) => {
+            // Entfernt Nicht-ASCII-Zeichen aus den empfangenen Daten.
             data = data.replace(/[^\x00-\x7F]/g, '');
-            console.log(data.split(/\s{2,}/));
-        })
+            const dataArray = data.split(/\s{2,}/);
+            // Verarbeitet die empfangenen Daten
+            this.handleData(dataArray);
+        });
 
-        this.port.on('end', () => {
-            console.log('No more data');
+        this.port.on('close', () => {
+            this.logger.log('WARN', 'Der serielle Port wurde geschlossen.');
+        });
+
+        this.port.on('error', (err) => {
+            this.logger.log('ERROR', 'Fehler beim Lesen des seriellen Ports: ' + err.message);
+        });
+    }
+
+    stop() {
+        this.port.close((err) => {
+            if (err) {
+                this.logger.log('ERROR', 'Fehler beim Schließen des seriellen Ports: ' + err.message);
+                return;
+            }
+            this.logger.log('INFO', 'Serieller Port geschlossen.');
         });
     }
 }
