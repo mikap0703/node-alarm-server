0d2a25548946a99e4c05de982b15a582d187dfe9
mikap0703 <mika.petak@gmail.com>
2023-04-18 23:14:42 +0200

    removing configChecker from alarmHandler

--- alarmhandler.js
+++ alarmhandler.js
@@ -1,17 +1,12 @@
-import path from "path";
 import DiveraHandler from './apiHandlers/diveraHandler.js';
 import AlamosHandler from './apiHandlers/alamosHandler.js';
 import MailHandler from "./emailHandler.js";
 import DMEHandler from "./dmeHandler.js";
-import configChecker from "./config.js";
-import fs from "fs";
 
 export default class AlarmHandler {
     constructor(config, logger) {
         this.config = config
         this.logger = logger
-
-        this.configChecker = new configChecker();
 
         this.doTriggerAlarm = this.config.general.alarm
 
@@ -44,26 +39,6 @@ export default class AlarmHandler {
             this.dmeHandler.start()
             this.logger.log('WARN', 'SERIAL DME - Auswertung noch nicht implementiert')
         }
-    }
-
-    async loadConfig() {
-        const configFolder = process.env.DEV_CONFIG_PATH || './config';
-        const configDir = path.join(this.dirname, configFolder);
-
-        await this.configChecker.check(configDir);
-
-        this.config = this.configChecker;
-
-        const mailConfigPath = path.join(configDir, 'mail.json');
-        this.config.mail = JSON.parse(fs.readFileSync(mailConfigPath));
-
-        const serialDmeConfigPath = path.join(configDir, 'serial-dme.json');
-        this.config.serialDME = JSON.parse(fs.readFileSync(serialDmeConfigPath));
-
-        const generalConfigPath = path.join(configDir, 'general.json');
-        this.config.general = JSON.parse(fs.readFileSync(generalConfigPath));
-
-        this.logger.log('INFO', 'Konfiguration ' + configDir + ' wurde geladen...');
     }
 
     handleAlarm(alarmInfo) {
