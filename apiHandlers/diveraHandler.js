b929d7b4289fed5da9d2fecaf8fb2fb9eac833ad
mikap0703 <mika.petak@gmail.com>
2023-04-27 19:57:56 +0200

    improved error handling when triggering alarm

--- apiHandlers/diveraHandler.js
+++ apiHandlers/diveraHandler.js
@@ -21,45 +21,43 @@ export default class DiveraHandler {
             address = alarmInfo.address.city
         }
 
-        if (process.env.DEV_MODE == 1) { // DEV
-            console.log(alarmInfo)
-        }
-        else {
-            axios.post('https://app.divera247.com/api/v2/alarms', {
-                accesskey: this.apikey,
-                Alarm: {
-                    priority: true,
-                    notification_type: this.generalConfig.diveraSettings.notificationType, // Empfänger-Auswahl (1 = Ausgewählte Standorte (nur in der PRO-Version), 2 = Alle des Standortes, 3 = Ausgewählte Gruppen, 4 = Ausgewählte Benutzer)
-                    foreign_id: alarmInfo.id,
-                    send_push: true,
-                    title: alarmInfo.title,
-                    text: alarmInfo.text + '\n' + alarmInfo.address.info,
-                    address: address,
-                    scene_object: alarmInfo.address.object,
-                    group: alarmInfo.groups,
-                    vehicle: alarmInfo.vehicles
+        axios.post('https://app.divera247.com/api/v2/alarms', {
+            accesskey: this.apikey,
+            Alarm: {
+                priority: true,
+                notification_type: this.generalConfig.diveraSettings.notificationType, // Empfänger-Auswahl (1 = Ausgewählte Standorte (nur in der PRO-Version), 2 = Alle des Standortes, 3 = Ausgewählte Gruppen, 4 = Ausgewählte Benutzer)
+                foreign_id: alarmInfo.id,
+                send_push: true,
+                title: alarmInfo.title,
+                text: alarmInfo.text + '\n' + alarmInfo.address.info,
+                address: address,
+                scene_object: alarmInfo.address.object,
+                group: alarmInfo.groups,
+                vehicle: alarmInfo.vehicles
+            },
+            instructions: {
+                group: {
+                    mapping: "title"
                 },
-                instructions: {
-                    group: {
-                        mapping: "title"
-                    },
-                    vehicle: {
-                        mapping: "name"
+                vehicle: {
+                    mapping: "name"
+                }
+            }
+        })
+            .then((response) => {
+                if (response.data.success) {
+                    this.logger.log('INFO', 'Alarm wurde erfolgreich ausgelöst!');
+                }
+                else {
+                    this.logger.log('ERROR', 'Beim Auslösen des Alarms sind folgende Fehler aufgetreten:');
+                    for (let notification of response.data.errors.notification_type) {
+                        this.logger.log('ERROR', notification)
                     }
                 }
             })
-                .then((response) => {
-                    if (response.data.success) {
-                        this.logger.log('INFO', 'Alarm wurde erfolgreich ausgelöst!');
-                    }
-                    else {
-                        this.logger.log('ERROR', 'Beim Auslösen des Alarms ist ein Fehler aufgetreten!');
-                    }
-                })
-                .catch((error) => {
-                    this.logger.log('ERROR', 'Alarm konnte nicht ausgelöst werden: ' + this.logger.convertObject(error));
-                });
-        }
+            .catch((error) => {
+                this.logger.log('ERROR', 'Alarm konnte nicht ausgelöst werden: ' + this.logger.convertObject(error));
+            });
     }
 
     checkConnection() {
