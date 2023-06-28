aae7eedf3d4dfbad5ee3dc74d9877ff6db2b7815
Mika <38019054+mikap0703@users.noreply.github.com>
2023-06-29 01:17:24 +0200

    diveraHandler js -> ts

---
+++ src/apiHandlers/diveraHandler.ts
@@ -0,0 +1,70 @@
+import axios from "axios";
+import {ILogger} from "../logger.js";
+import apiHandler from "./apiHandler.js";
+import {Alarm} from "../types/Alarm.js";
+
+export default class DiveraHandler extends apiHandler {
+    triggerAlarm(alarm: Alarm) {
+        let address = ""
+        if (alarm.address.street != '') {
+            if (alarm.address.city != '') {
+                address = alarm.address.street + ', ' + alarm.address.city;
+            } else {
+                address = alarm.address.street
+            }
+        } else {
+            address = alarm.address.city
+        }
+
+        axios.post('https://app.divera247.com/api/v2/alarms', {
+            accesskey: this.apikey,
+            Alarm: {
+                priority: true,
+                notification_type: this.generalConfig.diveraSettings.notificationType, // Empfänger-Auswahl (1 = Ausgewählte Standorte (nur in der PRO-Version), 2 = Alle des Standortes, 3 = Ausgewählte Gruppen, 4 = Ausgewählte Benutzer)
+                foreign_id: alarm.id,
+                send_push: true,
+                title: alarm.title,
+                text: alarm.text + '\n' + alarm.address.info,
+                address: address,
+                scene_object: alarm.address.object,
+                group: alarm.groups,
+                vehicle: alarm.vehicles
+            },
+            instructions: {
+                group: {
+                    mapping: "title"
+                },
+                vehicle: {
+                    mapping: "name"
+                }
+            }
+        })
+            .then((response) => {
+                if (response.data.success) {
+                    this.logger.log('INFO', 'Alarm wurde erfolgreich ausgelöst!');
+                }
+                else {
+                    this.logger.log('ERROR', 'Beim Auslösen des Alarms sind folgende Fehler aufgetreten:');
+                    for (let notification of response.data.errors.notification_type) {
+                        this.logger.log('ERROR', notification)
+                    }
+                }
+            })
+            .catch((error) => {
+                this.logger.log('ERROR', 'Alarm konnte nicht ausgelöst werden: ' + this.logger.convertObject(error));
+            });
+    }
+
+    checkConnection() {
+        axios.get('https://app.divera247.com/api/v2/pull/all?', { params: {
+                accesskey: this.apikey
+            }})
+            .then((res) => {
+                this.instanceName = res.data.data.cluster.name;
+                this.logger.log('INFO', `Divera API erfolgreich authorisiert (${this.instanceName})`)
+            })
+            .catch((error) => {
+                this.logger.log('ERROR', 'Verbindung konnte nicht überprüft werden: ' + this.logger.convertObject(error));
+            });
+    }
+}
