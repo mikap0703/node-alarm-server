0096026e0e570196a0a6459bd61bb7f4195c97dd
Mika <38019054+mikap0703@users.noreply.github.com>
2023-06-29 01:17:15 +0200

    apiHandler js -> ts

---
+++ src/apiHandlers/apiHandler.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import {Alarm} from "../types/Alarm.js";
+import {ILogger} from "../logger.js";
+import {gConfig} from "../types/gConfig.js";
+
+export interface ApiHandler {
+    triggerAlarm(alarm: Alarm): void;
+    checkConnection(): void;
+}
+export default class apiHandler implements ApiHandler {
+    protected apikey: string;
+    protected logger: ILogger;
+    protected generalConfig: any;
+    public instanceName: string;
+    constructor(apiKey: string, logger: ILogger, generalConfig: gConfig) {
+        this.apikey = apiKey;
+        this.logger = logger;
+        this.generalConfig = generalConfig;
+        this.instanceName = "";
+        this.checkConnection();
+    }
+
+    triggerAlarm(a: Alarm) {
+        // Funktion, die den Alarm anhand des übergebenen Alarms auslöst
+    }
+
+    checkConnection() {
+        // Funktion, die die API testet... Verifizierung des API-Schlüssels
+    }
+}
