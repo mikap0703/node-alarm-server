604827f78564e60f1e6078a5d5536d08da61db65
Mika <38019054+mikap0703@users.noreply.github.com>
2023-06-29 13:02:07 +0200

    added config types

---
+++ src/types/Config.ts
@@ -0,0 +1,41 @@
+import {Alarm} from "./Alarm.js";
+
+export type generalConfig = {
+    api: "Divera" | "Alamos";
+    apiKey: string;
+    timeout: number;
+    serialDME: boolean;
+    mail: boolean;
+    alarm: boolean;
+    defaultAlarmTitle: string;
+    [key: string]: any;
+};
+
+export type mailConfig = {
+    user: string;
+    password: string;
+    host: string;
+    port: number;
+    tls: boolean;
+    maxAge: number;
+    alarmSender: string;
+    alarmSubject: string;
+    alarmTemplateKeywords: Record<string, string>;
+    mailSchema: string;
+    stichwoerter: Record<string, string>;
+}
+
+export type serialDMEConfig = {
+    port: string;
+    delimeter: string;
+    baudrate: number;
+    alarmList: string[];
+    rics: Record<string, string>
+}
+
+export type config = {
+    general: generalConfig;
+    mail: mailConfig;
+    serialDME: serialDMEConfig;
+    alarmTemplates: Alarm;
+}
