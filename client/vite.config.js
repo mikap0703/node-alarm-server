113c31cb30b7ca79fd6df11dc4918efd2c55c94a
Mika <38019054+mikap0703@users.noreply.github.com>
2023-08-08 13:15:18 +0200

    added docker for client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import {config} from "dotenv";
+import { config } from "dotenv";
 
 config();
+
+console.log(process.env["BACKEND_IP_PORT"])
 
 export default defineConfig({
 	server: {
 		proxy: {
 			'/api': {
-				target: process.env.BACKEND_IP_PORT,
-				changeOrigin: true,
-				secure: false,
-			}
+				target: process.env["BACKEND_IP_PORT"],
+				changeOrigin: true
+			},
 		}
 	},
 	plugins: [sveltekit()]
