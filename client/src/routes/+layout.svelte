655418ca0cc5356b31a8f9c44c49b86dcdfd89b5
Mika <38019054+mikap0703@users.noreply.github.com>
2023-08-10 02:40:06 +0200

    moved client to own repo with tauri

--- client/src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import "carbon-components-svelte/css/g100.css";
-    import UIShell from "$lib/UIShell.svelte";
-</script>
-
-<UIShell>
-    <slot />
-</UIShell>
--- client/src/routes/settings/store.js
+++
@@ -1,8 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const configuration = writable({
-    "general": {},
-    "mail": {},
-    "dme": {},
-    "alarmTemplates": {}
-});
