860c14570bc10b594cfc09a8be66da87fd3d2e83
Mika <38019054+mikap0703@users.noreply.github.com>
2023-08-18 00:31:37 +0200

    easier auth

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken";
 import {ILogger} from "./logger.js";
 import {EventEmitter} from "node:events";
 
-let adminSecret: string = process.env.API_ADMIN_SECRET || "";
+let preSharedSecret: string | undefined = process.env.API_ADMIN_SECRET;
 
 export default class Backend{
     private port: number;
@@ -19,13 +19,6 @@ export default class Backend{
         this.port = port;
         this.logger = logger;
         this.emitter = emitter;
-
-        /*
-        const file = join(dirname, "db", 'users.json');
-        const adapter = new JSONFile(file)
-        const defaultData = { users: [] }
-        this.userDB = new Low(adapter, defaultData)
-        */
 
         this.app = express();
 
@@ -47,55 +40,21 @@ export default class Backend{
         this.v1Router.post('/auth/login', async (req, res) => {
             let data = req.body;
 
-            if (data.password === process.env.API_ADMIN_SECRET) {
+            if (data.preSharedSecret === preSharedSecret) {
+                /*
                 let token = await jwt.sign({
                     "login-time": Math.floor(Date.now() / 1000),
                 }, adminSecret, { expiresIn: '5d' });
+                 */
 
-                return res.status(200).send({"msg": "Erfolgreich angemeldet!", "token": token});
+                return res.status(200).send({"msg": "Erfolgreich angemeldet!"});
             }
             else {
                 return res.status(401).send({"msg": "Falsches Passwort!"});
             }
         })
 
-        this.v1Router.post('/auth/signup', async (req, res) => {
-            return res.status(405).send();
-            /*
-
-            Aktuell nicht benötigt, da Anmeldung via Admin Passwort, nicht Username, Passwort
-
-            await this.userDB.read();
-            let data = req.body;
-
-            for (let i = 0; i < this.userDB.data.users.length; i++) {
-                if (this.userDB.data.users[i].username === data.username) {
-                    return res.status(409).send({"msg": `Username ${data.username} already exists`});
-                }
-            }
-
-            if (data.adminSecret === process.env.API_ADMIN_SECRET) {
-                bcrypt.hash(data.password, 10, async (err, hash) => {
-                    if (err) {
-                        this.logger.log("Error", err)
-                    } else {
-                        this.userDB.data.users.push({
-                            "username": data.username,
-                            "password": hash
-                        })
-
-                        await this.userDB.write();
-                        let token = jwt.sign({
-                            "username": data.username
-                        }, process.env.API_ADMIN_SECRET, { expiresIn: '5d' });
-                        return res.status(200).send({"msg": `Erfolgreich als ${data.username} angemeldet!`, "token": token});
-                    }
-                });
-            }
-             */
-        })
-
-        this.v1Router.post("/test/:type", this.authenticateToken, (req, res) => {
+        this.v1Router.post("/test/:type", this.authenticate, (req, res) => {
             let data = req.body;
 
             switch (req.params.type) {
@@ -120,7 +79,7 @@ export default class Backend{
             res.status(200).send({status: "ok"});
         });
 
-        this.v1Router.post("/settings", this.authenticateToken, (req, res) => {
+        this.v1Router.post("/settings", this.authenticate, (req, res) => {
 
         })
 
@@ -129,13 +88,12 @@ export default class Backend{
         });
     }
 
-    async authenticateToken (req: any, res: any, next: any) {
+    async authenticate (req: any, res: any, next: any) {
         const token = req.headers["authorization"];
 
         if (token == null) return res.status(401).send({"err": "Nicht authorisiert!"})
-        jwt.verify(token, adminSecret, (err: any) => {
-            if (err) return res.sendStatus(403)
-            next()
-        })
+        if (token == preSharedSecret && token != '') {
+            next();
+        }
     }
 }
