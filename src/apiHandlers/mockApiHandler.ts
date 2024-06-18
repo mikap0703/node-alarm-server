18aac992faa6c9eacc22a708d0c56ccb9cec9ba9
Mika <38019054+mikap0703@users.noreply.github.com>
2024-06-18 13:58:12 +0200

    less logging again

--- src/apiHandlers/mockApiHandler.ts
+++ src/apiHandlers/mockApiHandler.ts
@@ -5,13 +5,11 @@ export default class MockApiHandler extends apiHandler {
   triggerAlarm (alarmFactory: IAlarmFactory): void {
     this.logger.log('INFO', 'Trigger Alarm')
     const alarm: Alarm = alarmFactory.export()
-    this.logger.log('DEBUG', this.logger.convertObject(alarm))
   }
 
   updateAlarm (alarmFactory: IAlarmFactory): void {
     this.logger.log('INFO', 'Update Alarm')
     const alarm: Alarm = alarmFactory.export()
-    this.logger.log('INFO', this.logger.convertObject(alarm))
   }
 
   checkConnection (): void {
